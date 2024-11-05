fb2324579d34bef18a856e02f3b679a508acf9bf
Pdimitrow <pdimitrow99@gmail.com>
2024-11-05 21:40:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
+string answer;
+float result;
+Console.WriteLine("What shape would you like to find the area of ? Please enter 'm' for rectangle, or any other key for circle.");
+
+answer = Console.ReadLine();
+
+if (answer == "m")
+{
+    Console.WriteLine("Please enter the height of the rectangle.");
+    float height = float.Parse(Console.ReadLine());
+    Console.WriteLine("Please enter the width of the rectangle.");
+    float width = float.Parse(Console.ReadLine());
+
+    result = height * width;
+}
+else
+{
+    Console.WriteLine("Please enter the radius of the circle.");
+    float radius = float.Parse(Console.ReadLine());
+
+    result = (float)Math.PI * (radius * radius);
+}
+Console.WriteLine("The result is " + result);
+Console.ReadKey();
+
+
+    
