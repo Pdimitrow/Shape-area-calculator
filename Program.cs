2003d6268551ad748c85e61f2a7563a0b81c7963
Pdimitrow <pdimitrow99@gmail.com>
2024-11-07 22:55:22 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,15 @@
-﻿using System.ComponentModel.Design;
-using System.Diagnostics;
-
-string answer;
-float result;
-Console.WriteLine("What shape would you like to find the area of ? Please enter 'm' for rectangle, or any other key for circle.");
-
-answer = Console.ReadLine();
-
-if (answer == "m")
-{
-    Console.WriteLine("Please enter the height of the rectangle.");
-    float height = float.Parse(Console.ReadLine());
-    Console.WriteLine("Please enter the width of the rectangle.");
-    float width = float.Parse(Console.ReadLine());
-
-    result = height * width;
-}
-else
-{
-    Console.WriteLine("Please enter the radius of the circle.");
-    float radius = float.Parse(Console.ReadLine());
-
-    result = (float)Math.PI * (radius * radius);
-}
-Console.WriteLine("The result is " + result);
-Console.ReadKey();
+﻿
+Console.WriteLine("Welcome!");
+Console.ReadLine();
+Console.WriteLine("Enter the height of your Hexagon:");
+double height = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the width of your Hexagon:");
+double width = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("You'r perimeter is:");
+Console.WriteLine((Convert.ToDouble(height) + Convert.ToDouble(width)) * 2);
+Console.WriteLine("You'r area is: ");
+Console.WriteLine((Convert.ToDouble(height) * Convert.ToDouble(width)));
 
 
-    
+
+
